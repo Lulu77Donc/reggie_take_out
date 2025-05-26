47934802019c8dabb70346c24faee26466ce4be5
lijiaxiang_hela <1259465943@qq.com>
2025-05-26 15:01:33 +0800

    套餐删除

--- src/main/java/com/ljx/service/impl/SetmealServiceImpl.java
+++ src/main/java/com/ljx/service/impl/SetmealServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ljx.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ljx.common.CustomException;
 import com.ljx.dto.SetmealDto;
 import com.ljx.entity.Setmeal;
 import com.ljx.entity.SetmealDish;
@@ -41,5 +43,36 @@ public class SetmealServiceImpl extends ServiceImpl<SetmealMapper, Setmeal> impl
 
         //保存套餐和菜品关联信息，操作setmeal_dish，执行insert操作
         setmealDishService.saveBatch(setmealDishes);
+    }
+
+    /**
+     * 删除套餐，同时需要删除套餐和菜品关联数据
+     * @param ids
+     */
+    @Override
+    @Transactional
+    public void removeWithDish(List<Long> ids) {
+
+        //select count(*) from setmeal where id in (1,2,3) and status = 1
+        //查询套餐状态，确定是否可以删除
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.in(Setmeal::getId,ids);
+        queryWrapper.eq(Setmeal::getStatus,1);
+
+        long count = this.count(queryWrapper);
+        if(count > 0 ){
+            //如果不能删除，抛出一个业务异常
+            throw new CustomException("套餐正在售卖中，不能删除");
+        }
+
+        //如果可以删除，先删除套餐表的数据---setmeal
+        this.removeByIds(ids);
+
+        //删除关系表中的数据---setmeal_dish
+        //delete from setmeal_dish where setmeal_id in (1,2,3)
+        LambdaQueryWrapper<SetmealDish> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.in(SetmealDish::getSetmealId,ids);
+        setmealDishService.remove(lambdaQueryWrapper);
+
     }
 }
