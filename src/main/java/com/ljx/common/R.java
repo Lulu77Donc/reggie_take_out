0f4ca505a14d3b5fbe23db1bf0faae72d60d0f39
lijiaxiang_hela <1259465943@qq.com>
2025-05-31 16:49:33 +0800

    spring Cache缓存套餐数据

--- src/main/java/com/ljx/common/R.java
+++ src/main/java/com/ljx/common/R.java
@@ -1,13 +1,15 @@
 package com.ljx.common;
 
 import lombok.Data;
+
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 /*
 * 通用返回结果类，服务端相应的数据最终会封装此对象*/
 @Data
-public class R<T> {
+public class R<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
 
